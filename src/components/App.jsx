a9fd15a90283eebbad456bf28af9d3ef9b425e4b
Wilmer <wilmersinchi@gmail.com>
2021-02-25 19:06:50 -0500

    Router Implemtation v1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
-
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Header from './Header.jsx'
+import Main from './Main.jsx'
+// import SignUp from '../components/SignUp'
 const App = () => {
-  return <div>MacroTracker</div>;
+  return (
+    <BrowserRouter>
+     
+
+    <Header/>
+    <Switch>
+    <Route path = '/' component ={Main} exact={true}/>
+</Switch>
+
+    </BrowserRouter>
+   
+    
+  )
 };
 
 export default App;
